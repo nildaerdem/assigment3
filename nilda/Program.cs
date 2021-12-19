8d52a1dc42ff4433306d69c0988558d46fe43fcb
gorka <gorka@DESKTOP-902BBMO>
2021-12-19 18:47:27 +0100

    Proje dosyası ekle.

---
+++ nilda/Program.cs
@@ -0,0 +1,25 @@
+﻿int a, b, c, e;
+
+Console.Write("Enter a number:");
+a = Convert.ToInt32(Console.ReadLine());
+
+for (b = 2; b <= a; b++)
+{
+	e = 2;
+	c = 1;
+	while (e < b)
+	{
+		if (b % e == 0)
+		{
+			c = 0;
+			break;
+		}
+		e++;
+	}
+	if (c == 1)
+	{
+		Console.WriteLine("Number is prime:" + b);
+	}
+}
+
+	
